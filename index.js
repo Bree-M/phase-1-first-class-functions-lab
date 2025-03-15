2f7098e615a24e2846edf58e68afe70989d65e22
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-16 01:14:11 +0300

    first class function lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);
+};
+
+
+
+    const returnLastTwoDrivers = (drivers) => {
+        return drivers.slice(2,4);
+    };
+
+
+
+
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+    
+    const createFareMultiplier = (multiplier) => {
+    return (fare) => fare * multiplier;
+
+    };
+
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+
+    const selectDifferentDrivers = (drivers, driverSelector) => {
+        return driverSelector(drivers);
+    };
+
+    console.log(returnFirstTwoDrivers(['Tom', 'Jerry', 'Powerpuffgirls', 'Ricky'])); 
+    console.log(returnLastTwoDrivers(['Tom', 'Jerry', 'Powerpuffgirls', 'Ricky']));
+    console.log(fareDoubler(10));
+    console.log(fareTripler(10));
+    console.log(selectDifferentDrivers(['Tom','Jerry','Powerpuffgirls','Ricky'], returnFirstTwoDrivers));
+    console.log(selectDifferentDrivers(['Tom','Jerry','Powerpuffgirls','Ricky'], returnLastTwoDrivers));
